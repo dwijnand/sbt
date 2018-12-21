e0c89802e6d3e696b6784d73963031a714c54771
Dale Wijnand <dale.wijnand@gmail.com>
2018-12-21 07:59:03 +0000

    cleanup ParseKey

--- main/src/test/scala/ParseKey.scala
+++ main/src/test/scala/ParseKey.scala
@@ -87,10 +87,9 @@ object ParseKey extends SeededProperties("Key parser test") {
       val mask = skm.mask.copy(project = false)
       // skip when config axis is set to Zero
       val hasZeroConfig = key.scope.config == Zero
-      parseCheck(structure, key, mask)(
-        sk =>
-          (hasZeroConfig || sk.scope.project == Select(structure.current))
-            :| s"Current: ${structure.current}"
+      parseCheck(structure, key, mask)(sk =>
+        (hasZeroConfig || sk.scope.project == Select(structure.current))
+          :| s"Current: ${structure.current}"
       )
   }
 
